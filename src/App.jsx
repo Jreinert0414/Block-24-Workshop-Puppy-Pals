77290e50afdf5b6d71aac438cf6240f8534b80d5
Jreinert0414 <jim.reinert.ii@gmail.com>
2024-03-26 17:30:09 -0500

    homework attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import './App.css'
+import { puppyList } from "./data.js"
+import { useState } from "react"
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-
+  function handleClick() {
+    
+  }
+  const featuredPup = puppies.find((pup)=>pup.id===featPupId)
+  console.log(featuredPup)
   return (
-    <>
-      
-    </>
-  )
+
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{setFeatPupId(`puppy id: `,puppy.id)}}
+           key={puppy.id}>{puppy.name}
+           {featPupId && <p>{featPupId}</p>}
+          </p>
+        );
+      })}
+    </div>
+    
+    )
+ 
 }
 
 export default App
