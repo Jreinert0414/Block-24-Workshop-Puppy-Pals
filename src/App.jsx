e99855556b429dfd70b4fd0c2ed60dc9294b8907
Jreinert0414 <jim.reinert.ii@gmail.com>
2024-03-27 08:53:29 -0500

    even watchin your review i couldnt find the pictures

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,35 @@ import { useState } from "react"
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
   function handleClick() {
-    
   }
-  const featuredPup = puppies.find((pup)=>pup.id===featPupId)
+
   console.log(featuredPup)
   return (
-
-    <div className="App">
-      {puppies.map((puppy) => {
-        return (
-          <p onClick={()=>{setFeatPupId(`puppy id: `,puppy.id)}}
-           key={puppy.id}>{puppy.name}
-           {featPupId && <p>{featPupId}</p>}
-          </p>
-        );
-      })}
-    </div>
-    
-    )
- 
+    <>
+      <div>
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>email:{featPupId.email}</li>
+              <li>Age:{featPupId.age}</li>
+              <img className="puppy-img" src={featuredPup.image} />
+            </ul>
+          </div>
+        )}
+        {
+          puppies.map((puppy) => {
+            return <p onClick={() => { setFeatPupId(puppy.id) }}
+              key={puppy.id}>{puppy.name}</p>
+          })
+        }
+      </div>
+    </>
+  )
 }
+
+
 
 export default App
